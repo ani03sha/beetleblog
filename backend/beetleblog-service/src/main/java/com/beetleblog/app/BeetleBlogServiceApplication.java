3f5364158b3ea0a36eb9aac8089546e2e059c621
Anirudh Sharma <anirudh03sharma@gmail.com>
2021-02-22 19:43:24 +0530

    Initial Commit for Beetle Blog service

---
+++ backend/beetleblog-service/src/main/java/com/beetleblog/app/BeetleBlogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.beetleblog.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeetleBlogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeetleBlogServiceApplication.class, args);
+	}
+
+}
---
+++ backend/beetleblog-service/src/test/java/com/beetleblog/app/BeetleBlogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beetleblog.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeetleBlogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
